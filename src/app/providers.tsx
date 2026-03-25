4648e2d4ad741bc752bd60fe4742afea8628fa54
sohxxny <sohxxny@gmail.com>
2026-03-25 15:49:53 +0900

    refactor: 불필요한 라이브러리, provider 삭제 (#185)

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { OverlayProvider } from 'overlay-kit';
@@ -12,18 +11,14 @@ import { queryClient } from '@/lib/query-client';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <GoogleOAuthProvider
-        clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ''}
-      >
-        <OverlayProvider>
-          <Tooltip.Provider>
-            <NotificationProvider>{children}</NotificationProvider>
-            {process.env.NODE_ENV === 'development' && (
-              <ReactQueryDevtools initialIsOpen={false} />
-            )}
-          </Tooltip.Provider>
-        </OverlayProvider>
-      </GoogleOAuthProvider>
+      <OverlayProvider>
+        <Tooltip.Provider>
+          <NotificationProvider>{children}</NotificationProvider>
+          {process.env.NODE_ENV === 'development' && (
+            <ReactQueryDevtools initialIsOpen={false} />
+          )}
+        </Tooltip.Provider>
+      </OverlayProvider>
     </QueryClientProvider>
   );
 }
